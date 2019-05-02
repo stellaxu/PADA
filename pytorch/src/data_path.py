bfa2939a0a0967dbef4da26eeabd1fe2b40c120d
astrongstorm <caomichael19@gmail.com>
2019-05-02 20:31:27 +0800

    for Office-Home data type for Berkeley GPU

--- pytorch/src/data_path.py
+++ pytorch/src/data_path.py
@@ -1,14 +1,14 @@
 
 
-f = open("../data/office/amazon_31_list.txt", "r")
-my_path = "/home/ureca/chn/office31"
+f = open("../data/office-home/Real_World_shared.txt", "r")
+my_path = "/work/xuweinan/OfficeHomeDataset"
 r = f.readlines()
 for i in range(0, len(r)):
     print(r[i])
-    pre_path, rest = r[i].split('office', 1)
+    pre_path, rest = r[i].split('office-home', 1)
     r[i] = my_path + rest
     print("new: " + r[i])
-    f = open("../data/office/chn_amazon_31_list.txt", "a+")
+    f = open("../data/office-home/chn_Real_World_shared.txt", "a+")
     f.write(r[i])
     f.close()
 
