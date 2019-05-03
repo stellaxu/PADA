6bb385cac7009252b815a2a25cd82ed52ebb5f94
astrongstorm <caomichael19@gmail.com>
2019-05-03 19:20:51 +0800

    preparing to submit job in URECA server

--- pytorch/src/data_path.py
+++ pytorch/src/data_path.py
@@ -1,14 +1,14 @@
 
 
-f = open("../data/office-home/Real_World_shared.txt", "r")
-my_path = "/work/xuweinan/OfficeHomeDataset"
+f = open("../data/office-home/Art_shared.txt", "r")
+my_path = "/home/ureca/chn/OfficeHomeDataset"
 r = f.readlines()
 for i in range(0, len(r)):
     print(r[i])
     pre_path, rest = r[i].split('office-home', 1)
     r[i] = my_path + rest
     print("new: " + r[i])
-    f = open("../data/office-home/chn_Real_World_shared.txt", "a+")
+    f = open("../data/office-home/chn_Art_shared.txt", "a+")
     f.write(r[i])
     f.close()
 
